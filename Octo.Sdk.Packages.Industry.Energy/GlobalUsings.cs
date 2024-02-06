b6aa0399e49ab05c474bc38f079648b1dd1596be
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-02-06 15:59:27 +0100

    Construction kits fixed to compile again.

--- Octo.Sdk.Packages.Industry.Energy/GlobalUsings.cs
+++ Octo.Sdk.Packages.Industry.Energy/GlobalUsings.cs
@@ -1,1 +1,3 @@
 global using Octo.Sdk.Packages.Industry.Basic.ConstructionKit.Generated.IndustryBasic.v1;
+global using Octo.Sdk.Packages.Basic.ConstructionKit.Generated.Basic.v1;
+global using Meshmakers.Octo.Runtime.Contracts.RepositoryEntities;
--- Octo.Sdk.Packages.Industry.Water/GlobalUsings.cs
+++ Octo.Sdk.Packages.Industry.Water/GlobalUsings.cs
@@ -1,1 +1,2 @@
-global using Meshmakers.Octo.Runtime.Contracts.RepositoryEntities;
+global using Octo.Sdk.Packages.Industry.Basic.ConstructionKit.Generated.IndustryBasic.v1;
+global using Octo.Sdk.Packages.Basic.ConstructionKit.Generated.Basic.v1;

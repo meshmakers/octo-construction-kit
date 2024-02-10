2e0d41924c73c68318605e1165c2b0aba8b25f9e
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-02-10 16:57:29 +0100

    AB#994 Create samples for pools/plugs/sockets

--- src/ck/om_importrt_sample_maintenance.ps1
+++ src/ck/om_importrt_sample_maintenance.ps1
@@ -1,2 +1,2 @@
-#octo-cli -c ImportRt -f ./application/samples/maintenance/rt-plug.json -w
+#octo-cli -c ImportRt -f ./../../Octo.Sdk.Packages.Industry.Basic/Samples/rt-plugs.yaml -w
 octo-cli -c ImportRt -f ./../../Octo.Sdk.Packages.Industry.Basic/Samples/rt-maintenance.yaml -w
--- src/ck/om_login_k8_staging.ps1
+++ src/ck/om_login_k8_staging.ps1
@@ -1,5 +1,5 @@
 
-octo-cli -c Config -asu "https://assets.staging.meshmakers.cloud/" -isu "https://connect.staging.meshmakers.cloud" -bsu "https://bots.staging.meshmakers.cloud/" -tid "meshtest"
+octo-cli -c Config -asu "https://assets.staging.meshmakers.cloud/" -isu "https://connect.staging.meshmakers.cloud" -bsu "https://bots.staging.meshmakers.cloud/" -csu "https://communication.staging.meshmakers.cloud/" -tid "meshtest"
 octo-cli -c Login -i
 
 

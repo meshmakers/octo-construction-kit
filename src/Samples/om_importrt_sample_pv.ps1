8036d1a11ad825b8bf816e982ce39d8fa0c68318
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-02-14 11:43:28 +0100

    AB#994: Cleanup repo to follow guidelines; Reworked sample data for version 3 in area PV

---
+++ src/Samples/om_importrt_sample_pv.ps1
@@ -0,0 +1,3 @@
+#octo-cli -c ImportRt -f ./application/samples/pv/rt-plug.json -w
+#octo-cli -c ImportRt -f ./application/samples/pv/rt-pv.json -w
+octo-cli -c ImportRt -f ./ov/rt-plugs.yaml -w
--- src/ck/om_importrt_sample_maintenance.ps1
+++
@@ -1,2 +0,0 @@
-#octo-cli -c ImportRt -f ./../../Octo.Sdk.Packages.Industry.Basic/Samples/rt-plugs.yaml -w
-octo-cli -c ImportRt -f ./../../Octo.Sdk.Packages.Industry.Basic/Samples/rt-maintenance.yaml -w

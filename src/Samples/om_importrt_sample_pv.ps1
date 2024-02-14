61be120b39a08da2d9813b6d79677e9e19453b87
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-02-14 19:30:32 +0100

    AB#994: Updates to samples to ensure that they are conform to recent version

--- src/Samples/om_importrt_sample_pv.ps1
+++ src/Samples/om_importrt_sample_pv.ps1
@@ -1,3 +1,4 @@
 #octo-cli -c ImportRt -f ./application/samples/pv/rt-plug.json -w
 #octo-cli -c ImportRt -f ./application/samples/pv/rt-pv.json -w
-octo-cli -c ImportRt -f ./ov/rt-plugs.yaml -w
+octo-cli -c ImportRt -f ./pv/rt-pv.yaml -w
+octo-cli -c ImportRt -f ./pv/rt-plugs.yaml -w

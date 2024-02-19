7c9397fdd3d2122cf2e7f2753802811edcc2e06a
Reimar Klammer <reimar.klammer@meshmakers.io>
2024-02-19 17:08:22 +0100

    fix paths

--- src/Samples/om_importck.ps1
+++ src/Samples/om_importck.ps1
@@ -1,4 +1,8 @@
-octo-cli -c importck -f ./../../Octo.Sdk.Packages.Basic/ConstructionKit/ck-basic.yaml -w
-octo-cli -c importck -f ./../../Octo.Sdk.Packages.Industry.Basic/ConstructionKit/ck-industrybasic.yaml -w
-octo-cli -c importck -f ./../../Octo.Sdk.Packages.Industry.Energy/ConstructionKit/ck-industryenergy.yaml -w
-octo-cli -c importck -f ./../../Octo.Sdk.Packages.Industry.Water/ConstructionKit/ck-industrywater.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Basic/ConstructionKit/ck-basic.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Basic/ConstructionKit/ck-industrybasic.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Energy/ConstructionKit/ck-industryenergy.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Water/ConstructionKit/ck-industrywater.yaml -w
+
+# add the communication construction kit. beacause this is just a sample, it is okay to hardcode to another repository
+# anyways the communication controller should import this construction kit when the tenant is activated
+octo-cli -c importck -f ../../../octo-communication-controller-services/src/SystemCommunicationCkModel/ConstructionKit/ck-system.communication.yaml -w

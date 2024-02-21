9efb96a4ce912dabdb89923c085080dfb0a4afdc
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-02-21 20:38:52 +0100

    AB#995: Removed structure of plugs/sockets, consolidated it to adapters. Added definitions for simulation plug. Still existing demos with modbus need adaptions.

--- src/Samples/om_importrt_sample_maintenance.ps1
+++ src/Samples/om_importrt_sample_maintenance.ps1
@@ -1,2 +1,2 @@
 octo-cli -c ImportRt -f ./maintenance/rt-maintenance.yaml -w
-octo-cli -c ImportRt -f ./maintenance/rt-plugs.yaml -w
+octo-cli -c ImportRt -f ./maintenance/rt-adapters.yaml -w
---
+++ src/Samples/om_importrt_sample_simulation.ps1
@@ -0,0 +1,3 @@
+
+#octo-cli -c ImportRt -f ./simulator/rt-pv.yaml -w
+octo-cli -c ImportRt -f ./simulator/rt-adapters.yaml -w

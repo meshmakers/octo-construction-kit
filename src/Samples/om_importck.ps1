a1b5bded462522d4b0232b32cb6da2b123d799bf
reimarklammer <reimar.klammer+github@gmail.com>
2024-02-27 20:30:46 +0100

    now really

--- src/Samples/om_importck.ps1
+++ src/Samples/om_importck.ps1
@@ -1,7 +1,7 @@
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Basic/ConstructionKit/ck-basic.yaml -w
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Basic/ConstructionKit/ck-industrybasic.yaml -w
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Energy/ConstructionKit/ck-industryenergy.yaml -w
-octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Water/ConstructionKit/ck-industrywater.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Fluid/ConstructionKit/ck-industryfluid.yaml -w
 
 # add the communication construction kit. beacause this is just a sample, it is okay to hardcode to another repository
 # anyways the communication controller should import this construction kit when the tenant is activated

a20bc84e1d1041dc25d74857be239ec380473ad5
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-03-04 13:06:59 +0100

    AB#994: Fix: The communication controller ck was imported, which was a bug.

--- src/Samples/om_importck.ps1
+++ src/Samples/om_importck.ps1
@@ -2,7 +2,3 @@ octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Basic/Construction
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Basic/ConstructionKit/ck-industrybasic.yaml -w
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Energy/ConstructionKit/ck-industryenergy.yaml -w
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Fluid/ConstructionKit/ck-industryfluid.yaml -w
-
-# add the communication construction kit. beacause this is just a sample, it is okay to hardcode to another repository
-# anyways the communication controller should import this construction kit when the tenant is activated
-octo-cli -c importck -f ../../../octo-communication-controller-services/src/SystemCommunicationCkModel/ConstructionKit/ck-system.communication.yaml -w

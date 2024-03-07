06d8a703d10e5278708118ffc5da9d4363cda702
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-03-07 09:38:51 +0100

    AB#1115: Reworked the naming of industry construction kits. IndustryBasic, IndustryEnergy -> Industry.Basic, Industry.Energy.

--- src/ConstructionKits/Octo.Sdk.Packages.Industry.Fluid/GlobalUsings.cs
+++ src/ConstructionKits/Octo.Sdk.Packages.Industry.Fluid/GlobalUsings.cs
@@ -1,2 +1,2 @@
-global using Octo.Sdk.Packages.Industry.Basic.ConstructionKit.Generated.IndustryBasic.v1;
+global using Octo.Sdk.Packages.Industry.Basic.ConstructionKit.Generated.Industry.Basic.v1;
 global using Octo.Sdk.Packages.Basic.ConstructionKit.Generated.Basic.v1;
--- src/Samples/om_importck.ps1
+++ src/Samples/om_importck.ps1
@@ -1,4 +1,4 @@
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Basic/ConstructionKit/ck-basic.yaml -w
-octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Basic/ConstructionKit/ck-industrybasic.yaml -w
-octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Energy/ConstructionKit/ck-industryenergy.yaml -w
-octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Fluid/ConstructionKit/ck-industryfluid.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Basic/ConstructionKit/ck-industry.basic.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Energy/ConstructionKit/ck-industry.energy.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Fluid/ConstructionKit/ck-industry.fluid.yaml -w

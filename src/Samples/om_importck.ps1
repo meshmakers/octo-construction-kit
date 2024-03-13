c45ee3609cbb9665b12559a32d18689e30abd907
Reimar Klammer <reimar.klammer@meshmakers.io>
2024-03-13 17:40:12 +0100

    Script respects now the configuration. Defaults to Release, but can be changed to Debug/DebugL

--- src/Samples/om_importck.ps1
+++ src/Samples/om_importck.ps1
@@ -1,4 +1,14 @@
-octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Basic/bin/Release/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Basic/out/ck-basic.yaml -w
-octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Basic/bin/Release/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Basic/out/ck-industry.basic.yaml -w
-octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Energy/bin/Release/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Energy/out/ck-industry.energy.yaml -w
-octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Fluid/bin/Release/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Fluid/out/ck-industry.fluid.yaml -w
+param (
+    [string]$configuration = "Release"
+)
+
+if (!(Test-Path -Path $PSScriptRoot/../ConstructionKits/Octo.Sdk.Packages.Basic/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Basic/out/ck-basic.yaml)) {
+    Write-Host "Octo.Sdk.Packages.Basic Construction Kit not found at $PSScriptRoot/../ConstructionKits/Octo.Sdk.Packages.Basic/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Basic/out/ck-basic.yaml"
+    exit 1
+}
+
+
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Basic/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Basic/out/ck-basic.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Basic/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Basic/out/ck-industry.basic.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Energy/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Energy/out/ck-industry.energy.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Fluid/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Fluid/out/ck-industry.fluid.yaml -w

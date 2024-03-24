58917fa2fae535fd556a11225829fb0ceaf25bf5
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-03-24 20:54:52 +0100

    AB#1088: New: Update using mesh adapters now

--- src/Samples/om_importrt_sample_simulation.ps1
+++ src/Samples/om_importrt_sample_simulation.ps1
@@ -1,4 +1,4 @@
 octo-cli -c EnableCommunication
-octo-cli -c EnableTimeseries
 octo-cli -c ImportRt -f ./simulator/rt-simulator.yaml -w
-octo-cli -c ImportRt -f ./simulator/rt-adapters.yaml -w
+octo-cli -c ImportRt -f ./simulator/rt-adapters-mesh.yaml -w
+octo-cli -c ImportRt -f ./simulator/rt-adapters-edge.yaml -w

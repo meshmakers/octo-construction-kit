a38858a67648a1933cba4f880955a31d86a29a3b
Reimar Klammer <reimar.klammer@meshmakers.io>
2024-04-15 17:18:22 +0200

    remove timeseries

--- src/Samples/om_login_local.ps1
+++ src/Samples/om_login_local.ps1
@@ -1,5 +1,3 @@
 
-octo-cli -c Config -asu "https://localhost:5001/" -isu "https://localhost:5003/" -bsu "https://localhost:5009/" -csu "https://localhost:5015/" -tsu "https://localhost:5013/" -tid "meshtest"
+octo-cli -c Config -asu "https://localhost:5001/" -isu "https://localhost:5003/" -bsu "https://localhost:5009/" -csu "https://localhost:5015/" -tid "meshtest"
 octo-cli -c Login -i
-
-

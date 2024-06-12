f8d35738452b67b82f8cae8f3888bc9abac2b218
meshmakers <request@meshmakers.io>
2024-06-12 11:16:29 +0200

    AB#1320: New: Login script for rke2-local

---
+++ src/Samples/om_login_k8_rke2-local.ps1
@@ -0,0 +1,5 @@
+
+octo-cli -c Config -asu "https://assets.rke2-local.srv.mm.local/" -isu "https://connect.rke2-local.srv.mm.local" -bsu "https://bots.rke2-local.srv.mm.local/" -csu "https://communication.rke2-local.srv.mm.local/" -tid "meshtest"
+octo-cli -c Login -i
+
+

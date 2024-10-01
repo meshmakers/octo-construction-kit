ef3af69e03840fc252352cc6d05b1b5e1e971c14
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-10-01 18:47:27 +0200

    AB#1490: New: Added first version of maintenance construction kit.

--- src/ConstructionKits/Octo.Sdk.Packages.Industry.Maintenance/GlobalUsings.cs
+++ src/ConstructionKits/Octo.Sdk.Packages.Industry.Maintenance/GlobalUsings.cs
@@ -1,1 +1,3 @@
 global using Meshmakers.Octo.Runtime.Contracts.RepositoryEntities;
+global using Octo.Sdk.Packages.Basic.Generated.Basic.v1;
+global using Octo.Sdk.Packages.Industry.Basic.Generated.Industry.Basic.v1;

963e8eca07b8e35a326103aff74d09b3bf25b4e9
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-11-06 21:33:59 +0100

    AB#1504: Fix: Removed not needed ck types. updated import ck script to import maintenance ck too.

--- src/Samples/om_importck.ps1
+++ src/Samples/om_importck.ps1
@@ -12,5 +12,6 @@ octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Basic/bin/$configu
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Basic/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Basic/out/ck-industry.basic.yaml -w
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Energy/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Energy/out/ck-industry.energy.yaml -w
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Fluid/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Fluid/out/ck-industry.fluid.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Maintenance/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Maintenance/out/ck-industry.maintenance.yaml -w
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Environment/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Environment/out/ck-environment.yaml -w
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Demo/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Demo/out/ck-octosdkdemo.yaml -w

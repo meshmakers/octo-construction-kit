eb29da5eaabe08320d09edb07b1178ffaf51ab2d
PhilippSchwaab <159003947+PhilippSchwaab@users.noreply.github.com>
2024-12-03 10:18:40 +0100

    AB#1575: Changes to EnergyCommunityCK (#6)
    * AB#1575: Add Reference to System.Identity UserLogin Record
    
    * AB#1575: Importing System.Identity/User Directly to Customer.yaml type
    
    * AB#1575: Create Association between System.Identity/User and Customer.yaml
    
    * AB#1575: Rework energyCommunities CK to EnergyCommunityCk. Added ZeroOrOne for Testing Purposes!
    
    * AB#1575: Add Metadata and descriptions
    
    * AB#1575: Correct Ck Name in azure-pipelines.yml
    
    * AB#1575: Change Authentication Email to use Basic/EMailAddress and make MeteringPoint.yaml abstract
    
    * AB#1575: Add isOptional to CompanyRegisterNumberAttribute
    
    ---------
    
    Co-authored-by: PhilippSchwaab <philipp.schwaab@meshmakers.io>

--- src/Samples/om_importck.ps1
+++ src/Samples/om_importck.ps1
@@ -9,6 +9,7 @@ if (!(Test-Path -Path $PSScriptRoot/../ConstructionKits/Octo.Sdk.Packages.Basic/
 
 
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Basic/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Basic/out/ck-basic.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.EnergyCommunity/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.EnergyCommunity/out/ck-energycommunity.yaml -w
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Basic/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Basic/out/ck-industry.basic.yaml -w
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Energy/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Energy/out/ck-industry.energy.yaml -w
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Fluid/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Fluid/out/ck-industry.fluid.yaml -w
--- src/Samples/om_publish_github.ps1
+++ src/Samples/om_publish_github.ps1
@@ -8,7 +8,8 @@ if (!(Test-Path -Path $PSScriptRoot/../ConstructionKits/Octo.Sdk.Packages.Basic/
 }
 
 
-octo-ckc -c Publish -rep "PublicGitHubRepository" -r -f ../ConstructionKits/Octo.Sdk.Packages.Basic/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Basic/out/ck-basic.yaml 
+octo-ckc -c Publish -rep "PublicGitHubRepository" -r -f ../ConstructionKits/Octo.Sdk.Packages.Basic/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Basic/out/ck-basic.yaml
+octo-ckc -c Publish -rep "PublicGitHubRepository" -r -f ../ConstructionKits/Octo.Sdk.Packages.EnergyCommunity/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.EnergyCommunity/out/ck-energycommunity.yaml
 octo-ckc -c Publish -rep "PublicGitHubRepository" -r -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Basic/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Basic/out/ck-industry.basic.yaml 
 octo-ckc -c Publish -rep "PublicGitHubRepository" -r -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Energy/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Energy/out/ck-industry.energy.yaml 
 octo-ckc -c Publish -rep "PublicGitHubRepository" -r -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Fluid/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Fluid/out/ck-industry.fluid.yaml 

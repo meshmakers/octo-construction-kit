dda8237bd967b241a5472e0773af0303af55521e
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-01-08 14:53:28 +0100

    AB#1677: Upgrade to .NET 9

--- src/Samples/om_importck.ps1
+++ src/Samples/om_importck.ps1
@@ -2,17 +2,17 @@ param (
     [string]$configuration = "Release"
 )
 
-if (!(Test-Path -Path $PSScriptRoot/../ConstructionKits/Octo.Sdk.Packages.Basic/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Basic/out/ck-basic.yaml)) {
-    Write-Host "Octo.Sdk.Packages.Basic Construction Kit not found at $PSScriptRoot/../ConstructionKits/Octo.Sdk.Packages.Basic/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Basic/out/ck-basic.yaml"
+if (!(Test-Path -Path $PSScriptRoot/../ConstructionKits/Octo.Sdk.Packages.Basic/bin/$configuration/net9.0/octo-ck-libraries/Octo.Sdk.Packages.Basic/out/ck-basic.yaml)) {
+    Write-Host "Octo.Sdk.Packages.Basic Construction Kit not found at $PSScriptRoot/../ConstructionKits/Octo.Sdk.Packages.Basic/bin/$configuration/net9.0/octo-ck-libraries/Octo.Sdk.Packages.Basic/out/ck-basic.yaml"
     exit 1
 }
 
 
-octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Basic/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Basic/out/ck-basic.yaml -w
-octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.EnergyCommunity/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.EnergyCommunity/out/ck-energycommunity.yaml -w
-octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Basic/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Basic/out/ck-industry.basic.yaml -w
-octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Energy/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Energy/out/ck-industry.energy.yaml -w
-octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Fluid/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Fluid/out/ck-industry.fluid.yaml -w
-octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Maintenance/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Maintenance/out/ck-industry.maintenance.yaml -w
-octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Environment/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Packages.Environment/out/ck-environment.yaml -w
-octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Demo/bin/$configuration/net8.0/octo-ck-libraries/Octo.Sdk.Demo/out/ck-octosdkdemo.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Basic/bin/$configuration/net9.0/octo-ck-libraries/Octo.Sdk.Packages.Basic/out/ck-basic.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.EnergyCommunity/bin/$configuration/net9.0/octo-ck-libraries/Octo.Sdk.Packages.EnergyCommunity/out/ck-energycommunity.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Basic/bin/$configuration/net9.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Basic/out/ck-industry.basic.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Energy/bin/$configuration/net9.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Energy/out/ck-industry.energy.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Fluid/bin/$configuration/net9.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Fluid/out/ck-industry.fluid.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Maintenance/bin/$configuration/net9.0/octo-ck-libraries/Octo.Sdk.Packages.Industry.Maintenance/out/ck-industry.maintenance.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Environment/bin/$configuration/net9.0/octo-ck-libraries/Octo.Sdk.Packages.Environment/out/ck-environment.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Demo/bin/$configuration/net9.0/octo-ck-libraries/Octo.Sdk.Demo/out/ck-octosdkdemo.yaml -w

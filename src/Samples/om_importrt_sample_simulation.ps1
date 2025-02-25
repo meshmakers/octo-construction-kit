4d6dd476202baac81520eae475be0683f15c75b7
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-02-25 18:58:10 +0100

    AB#1853: New: Added sample to create customers of octo sdk construction kit

--- src/Samples/om_importrt_sample_simulation.ps1
+++ src/Samples/om_importrt_sample_simulation.ps1
@@ -1,2 +1,3 @@
 octo-cli -c ImportRt -f ./simulator/rt-simulator.yaml -w
+octo-cli -c ImportRt -f ./simulator/rt-create-customers.yaml -w
 octo-cli -c ImportRt -f ./simulator/rt-adapters-edge.yaml -w

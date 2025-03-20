44977ddbf2285b3617bd5a723daf1f5a2c9535d8
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-03-20 11:46:34 +0100

    AB#1887: New: Added URI of reporting services.

--- src/Samples/om_login_k8_staging.ps1
+++ src/Samples/om_login_k8_staging.ps1
@@ -1,7 +1,7 @@
 param (
     [string]$tenantId = "meshtest"
 )
-octo-cli -c Config -asu "https://assets.staging.meshmakers.cloud/" -isu "https://connect.staging.meshmakers.cloud" -bsu "https://bots.staging.meshmakers.cloud/" -csu "https://communication.staging.meshmakers.cloud/" -tid $tenantId
+octo-cli -c Config -asu "https://assets.staging.meshmakers.cloud/" -isu "https://connect.staging.meshmakers.cloud" -bsu "https://bots.staging.meshmakers.cloud/" -csu "https://communication.staging.meshmakers.cloud/" -rsu "https://reporting.staging.meshmakers.cloud/"  -tid $tenantId
 octo-cli -c Login -i
 
 
--- src/Samples/om_login_local.ps1
+++ src/Samples/om_login_local.ps1
@@ -1,5 +1,5 @@
 param (
     [string]$tenantId = "meshtest"
 )
-octo-cli -c Config -asu "https://localhost:5001/" -isu "https://localhost:5003/" -bsu "https://localhost:5009/" -csu "https://localhost:5015/" -tid $tenantId
+octo-cli -c Config -asu "https://localhost:5001/" -isu "https://localhost:5003/" -bsu "https://localhost:5009/" -csu "https://localhost:5015/" -rsu "https://localhost:5007/" -tid $tenantId
 octo-cli -c Login -i

dde1efed71d25e7a56aa6304f39ac9a3a15ad25f
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-07-24 12:05:53 +0200

    New: Make Nuget packages available (again)

--- src/ConstructionKits/Octo.Sdk.Packages.Environment/GlobalUsings.cs
+++ src/ConstructionKits/Octo.Sdk.Packages.Environment/GlobalUsings.cs
@@ -1,1 +1,1 @@
-global using Octo.Sdk.Packages.Basic.Generated.Basic.v1;
+global using Meshmakers.Octo.Sdk.Packages.Basic.Generated.Basic.v1;
--- src/ConstructionKits/Octo.Sdk.Packages.Industry.Maintenance/GlobalUsings.cs
+++ src/ConstructionKits/Octo.Sdk.Packages.Industry.Maintenance/GlobalUsings.cs
@@ -1,3 +1,3 @@
 global using Meshmakers.Octo.Runtime.Contracts.RepositoryEntities;
-global using Octo.Sdk.Packages.Basic.Generated.Basic.v1;
-global using Octo.Sdk.Packages.Industry.Basic.Generated.Industry.Basic.v1;
+global using Meshmakers.Octo.Sdk.Packages.Basic.Generated.Basic.v1;
+global using Meshmakers.Octo.Sdk.Packages.Industry.Basic.Generated.Industry.Basic.v1;

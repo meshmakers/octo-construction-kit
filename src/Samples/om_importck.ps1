202ce85d8deb0a7a12dcc8d7a5c25390f88b8ffc
Gerald Lochner <gerald.lochner@meshmakers.io>
2026-09-11 07:33:56 +0200

    Updated scripts

--- src/Samples/om_importck.ps1
+++ src/Samples/om_importck.ps1
@@ -10,7 +10,7 @@ if (!(Test-Path -Path $PSScriptRoot/../ConstructionKits/Octo.Sdk.Packages.Basic/
 
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Basic/bin/$configuration/$framework/octo-ck-libraries/Octo.Sdk.Packages.Basic/out/ck-basic-2.yaml -w
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Basic.Energy/bin/$configuration/$framework/octo-ck-libraries/Octo.Sdk.Packages.Basic.Energy/out/ck-basic.energy.yaml -w
-octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.EnergyCommunity/bin/$configuration/$framework/octo-ck-libraries/Octo.Sdk.Packages.EnergyCommunity/out/ck-energycommunity-3.yaml -w
+octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.EnergyCommunity/bin/$configuration/$framework/octo-ck-libraries/Octo.Sdk.Packages.EnergyCommunity/out/ck-energycommunity-4.yaml -w
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Basic/bin/$configuration/$framework/octo-ck-libraries/Octo.Sdk.Packages.Industry.Basic/out/ck-industry.basic-2.yaml -w
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Energy/bin/$configuration/$framework/octo-ck-libraries/Octo.Sdk.Packages.Industry.Energy/out/ck-industry.energy-3.yaml -w
 octo-cli -c importck -f ../ConstructionKits/Octo.Sdk.Packages.Industry.Fluid/bin/$configuration/$framework/octo-ck-libraries/Octo.Sdk.Packages.Industry.Fluid/out/ck-industry.fluid-2.yaml -w
